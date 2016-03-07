801b69e72f7c2c757d50d6648d51dc6f99ed72f2
Varun Mahajan <vamahaja@cisco.com>
2016-03-07 21:43:08 +0530

    Added examples of annotation based mappings

---
+++ HibernatePostgresql/src/main/java/org/bytegeeks/learn/Employee.java
@@ -0,0 +1,54 @@
+package org.bytegeeks.learn;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+import org.hibernate.annotations.Cascade;
+
+@Entity
+@Table(name = "EMPLOYEE")
+public class Employee {
+
+	@Id
+	@Column(name = "emp_id")
+	private long id;
+
+	@Column(name = "emp_name")
+	private String name;
+
+	@Column(name = "emp_salary")
+	private double salary;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+}
